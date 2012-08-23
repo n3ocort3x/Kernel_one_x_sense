892309eb3792eb40e936020739164ef519d33bc2
n3ocort3x <0xn3ocort3x@gmail.com>
2012-08-24 00:11:40 +0200

    revert some stuff and update htc_headset_gpio to latest source

--- arch/arm/mach-tegra/cpu-tegra3.c
+++ arch/arm/mach-tegra/cpu-tegra3.c
@@ -435,8 +435,8 @@ static void tegra_auto_cpuplug_work_func(struct work_struct *work)
 			if (cpu < nr_cpu_ids) {
 				up = false;
 				hp_stats_update(cpu, false);
-			} else if (!is_lp_cluster() && !no_lp) &&
-				!pm_qos_request(PM_QOS_MIN_ONLINE_CPUS)) { -
+			} else if (!is_lp_cluster() && !no_lp &&
+				!pm_qos_request(PM_QOS_MIN_ONLINE_CPUS)) {
 
 				/* Invalid request, why put sth down that is not there?
 				   This would cause a NULL pointer dereference in
